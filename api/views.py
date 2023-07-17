2abdd993f62a9ffca6d9153b5761f8c50f906ad6
AnujSingh13 <anujj.singh.13@gmail.com>
2023-07-18 00:05:48 +0530

    RestApi Commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+import nltk
+from nltk.corpus import stopwords
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Resume
+from .serializers import ResumeSerializer
+
+@api_view(['GET', 'POST'])
+def resume_list(request):
+    if request.method == 'GET':
+        resumes = Resume.objects.all()
+        serializer = ResumeSerializer(resumes, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ResumeSerializer(data=request.data)
+        if serializer.is_valid():
+            # Determine if the resume is for a backend or frontend developer
+            resume = request.FILES.get('resume')
+            text = resume.read().decode('utf-8')
+            developer_type = 'backend' if 'backend' in text.lower() else 'frontend'
+            serializer.save(developer_type=developer_type)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
